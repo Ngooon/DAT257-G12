cfe3869b3a91c2fb3413985c25b87c6c2825e849
Ngooon <55849179+Ngooon@users.noreply.github.com>
2025-04-10 13:53:15 +0200

    Add proxy in Angular and update endpoints and allowed hosts

--- backend/framework/settings.py
+++ backend/framework/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-r7fm7lx$(16wl6qq43-s*1wcbwum$z7&u)d(f26)-ps4ubv8*_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
--- backend/framework/urls.py
+++ backend/framework/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
-from framework.quickstart.views import (
-    get_garments,
-    create_garment,
-    update_garment,
-    delete_garment,
-    garments_view,
-)
+from framework.quickstart.views import garments_view
 
 
 urlpatterns = [
-    path("garments/", garments_view, name="garments_view"),
-    # path("garments/", create_garment, name="get_garment"),
-    # path("garments/create/", create_garment, name="create_garment"),
-    path("garments/<int:pk>/", garments_view, name="garments_view"),
-    # path("garments/<int:pk>/", garments_view, name="delete_garment"),
+    path("api/garments/", garments_view, name="garments_view"),
+    path("api/garments/<int:pk>/", garments_view, name="garments_view"),
 ]
