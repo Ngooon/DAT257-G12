d9a1269b257956fc479cae03b266af7359932aa1
Ngooon <55849179+Ngooon@users.noreply.github.com>
2025-04-20 21:03:34 +0200

    Dark/light mode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Wardrobe Curator Application';
+  currentTheme: string = 'auto';
+
+  setTheme(theme: string): void {
+    this.currentTheme = theme;
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+  }
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme') || this.currentTheme;
+    this.setTheme(savedTheme);
+  }
 }
