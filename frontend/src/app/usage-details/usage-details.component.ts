8d810ea652e23657cd81cdc9b89fe9f156b4e082
Ida Thorburn <idtho@student.chalmers.se>
2025-04-20 15:24:47 +0200

    usage details added, garment id not working

--- frontend/src/app/usage-details/usage-details.component.ts
+++ frontend/src/app/usage-details/usage-details.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface Usage {
+  id: number;
+  time: string;
+  garmentId: string;
+  notes: string;
+}
 
 @Component({
-  selector: 'app-usage-details',
-  standalone: false,
-  templateUrl: './usage-details.component.html',
-  styleUrl: './usage-details.component.css'
-})
-export class UsageDetailsComponent {
-
-}
+    selector: 'app-usage-details',
+    standalone: false,
+    templateUrl: './usage-details.component.html',
+    styleUrl: './usage-details.component.css'
+  })
+  export class UsageDetailsComponent implements OnInit {
+    public id: number = 0;
+    public usage: Usage | null = null;
+  
+    constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  
+    ngOnInit() {
+      const idString = this.route.snapshot.paramMap.get('id');
+      if (idString) {
+        this.id = parseInt(idString, 10);
+      }
+      this.getUsage();
+    }
+  
+    getUsage() {
+      this.http.get<Usage>(`/api/usages/${this.id}/`).subscribe(
+        (result) => {
+          this.usage = result;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
+  
