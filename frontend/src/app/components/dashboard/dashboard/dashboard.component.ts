44b2ed087f52e704a47170ee36846189b8109006
Ngooon <55849179+Ngooon@users.noreply.github.com>
2025-05-07 17:32:45 +0200

    Add charting functionality to dashboard with usage statistics

--- frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Listing } from '../../../interfaces/listing';
 import { Router } from '@angular/router';
 import { Garment } from '../../../interfaces/garment';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,11 +15,22 @@ export class DashboardComponent implements OnInit {
   public topUsages: Garment[] = [];
   listings: Listing[] = [];
 
-  constructor(private router: Router, private http: HttpClient) {}
+  public graphData: any = {};
+  public graphOptions = {
+    title: {
+      display: true,
+      text: 'Usage over time',
+      fontSize: 32,
+      position: 'top',
+    },
+  };
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
     this.getListings();
     this.fetchUsages();
+    this.plotStats();
   }
 
   getListings() {
@@ -33,7 +45,7 @@ export class DashboardComponent implements OnInit {
   }
 
   // public usages: Usage[] = []; // Alla usages hämtade från backend
-   // Top 10 mest använda plaggen
+  // Top 10 mest använda plaggen
 
 
   // Hämta alla usages från backend
@@ -47,5 +59,40 @@ export class DashboardComponent implements OnInit {
         console.error('Failed to load top usages from API.', error);
       }
     });
+  }
+
+
+  plotStats() {
+    this.http.get<any>(`/api/statistics/category/`).subscribe({
+      next: (result) => {
+        const labels = result.length > 0 ? Object.keys(result[0].statistics.usage_history) : [];
+        const datasets = result.map((category: any) => ({
+          label: category.name,
+          data: Object.values(category.statistics.usage_history),
+          fill: false,
+          // borderColor: '#000000', // Fixed color
+          tension: 0.1
+        }));
+
+        this.graphData = {
+          labels: labels,
+          datasets: datasets
+        };
+
+        // Force the graph to update
+        this.graphData = { ...this.graphData };
+      },
+      error: (error) => {
+        console.error('Error fetching category statistics:', error);
+      }
+    });
+  }
+
+  // Hjälpfunktion för att beräkna veckonummer
+  getWeekNumber(date: Date): string {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+    const weekNumber = Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+    return `Week ${weekNumber}`;
   }
 }
